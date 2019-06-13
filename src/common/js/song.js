20a20f2976e7fad0e696a75a8dec61ccc4b1a9b8
lijiatai <lijiataihpu@gmail.com>
2019-06-13 22:05:37 +0800

    歌词base64解码:zap:

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -1,5 +1,6 @@
 import { getSongsUrl, getLyric } from 'api/song'
 import { ERR_OK } from 'api/config'
+import {Base64} from 'js-base64'
 
 export default class Song {
     constructor({id, mid, singer, name, album, duration, image, url}) {
@@ -21,7 +22,8 @@ export default class Song {
         return new Promise((resolve, reject) => {
             getLyric(this.mid).then((res) => {
                 if (res.retcode === ERR_OK) {
-                    this.lyric = this.lyric
+                    this.lyric = Base64.decode(res.lyric)
+                    console.log(26, Base64.decode(res.lyric))
                     resolve(this.lyric)
                 } else {
                     reject('no lyric')
